e1bba3d092f39a30a135f1d433413ee6db13b2a1
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-03-16 17:00:17 -0400

    nav modified

--- js/script.js
+++ js/script.js
@@ -1,31 +1,13 @@
-var menuButton = document.querySelector("#menu");
-var ul = document.querySelector("#main-nav");
-var menuImg = document.querySelector("#menuImg");
-var nav = document.querySelector("nav");
-var li = document.querySelectorAll("li");
+// var nav = $('.navbar');
+// var projects;
 
-function expandMenu(){
-    if(menuButton.textContent=="X"){
-        closeMenu();
-    }else{
-        menuButton.textContent="X"
-        ul.style.display="block";
-        nav.style.justifyContent="space-around";
-        
-    }
-}
+// $(document).ready(function(){
 
-function closeMenu(){
-    menuButton.textContent="";
-    ul.style.display="none";
-    menuButton.appendChild(menuImg);
-    nav.style.justifyContent="flex-end";
-}
+//     $($('.navbar')[0]).on('activate.bs.scrollspy',function(){
+//         var li = $(" .navbar li.active > a ").text();
+//         if(li =="About"){
+//             alert("Hi hello");
+//         }
+//     });
 
-
-
-for (var i=0; i<li.length; i+=1){
-    li[i].addEventListener('click',closeMenu);
-}
-
-menuButton.addEventListener("click", expandMenu);
+// });
