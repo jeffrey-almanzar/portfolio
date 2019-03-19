61c23a3f2b2c416ef46d0d0c4e08f29f9d1e7cdc
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-03-18 21:14:30 -0400

    animations added

--- js/script.js
+++ js/script.js
@@ -7,7 +7,37 @@ sr.reveal('.navbar', {
     origin:'bottom'
 });
 
+sr.reveal('#welcome', {
+    duration:2000,
+    origin:'top',
+    distance:"300px"
+});
+
 sr.reveal('#about', {
-    duration:3000,
+    duration:2000,
     origin:'bottom'
+});
+
+sr.reveal('#pro-1', {
+    duration:2000,
+    origin:'left',
+    distance:'150px'
+});
+
+sr.reveal('#pro-2', {
+    duration:2000,
+    origin:'right',
+    distance:'150px'
+});
+
+sr.reveal('#pro-3', {
+    duration:2000,
+    origin:'left',
+    distance:'150px'
+});
+
+sr.reveal('#pro-4', {
+    duration:2000,
+    origin:'right',
+    distance:'150px'
 });
