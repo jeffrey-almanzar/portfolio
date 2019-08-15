8ea981fc0664be456b8e7c72439a464be408c730
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-08-14 21:20:16 -0400

    used prettier

--- js/script.js
+++ js/script.js
@@ -1,51 +1,47 @@
+$(document).ready(function() {
+  window.sr = ScrollReveal();
 
-$(document).ready(function(){
-    window.sr = ScrollReveal();
+  sr.reveal("#welcome", {
+    duration: 1500,
+    origin: "top",
+    distance: "200px"
+  });
 
-    sr.reveal('#welcome', {
-        duration:1500,
-        origin:'top',
-        distance:'200px'
-    });
+  //Type js
+  var typed = new Typed("#title-description", {
+    strings: ["Software engineer and computer science tutor."],
+    typeSpeed: 60,
+    showCursor: false,
+    startDelay: 100,
+    loop: true
+  });
 
+  sr.reveal("#about", {
+    duration: 2500,
+    origin: "bottom"
+  });
 
-    //Type js
-    var typed = new Typed("#title-description", {
-        strings: ["Software engineer and computer science tutor."],
-        typeSpeed: 60,
-        showCursor:false,
-        startDelay:100,
-        loop:true
-    });
+  sr.reveal("#pro-1", {
+    duration: 2000,
+    origin: "left",
+    distance: "30px"
+  });
 
-    sr.reveal('#about', {
-        duration:2500,
-        origin:'bottom'
-    });
+  sr.reveal("#pro-2", {
+    duration: 2000,
+    origin: "right",
+    distance: "30px"
+  });
 
-    sr.reveal('#pro-1', {
-        duration:2000,
-        origin:'left',
-        distance:'30px'
-    });
+  sr.reveal("#pro-3", {
+    duration: 2000,
+    origin: "left",
+    distance: "30px"
+  });
 
-    sr.reveal('#pro-2', {
-        duration:2000,
-        origin:'right',
-        distance:'30px'
-    });
-
-    sr.reveal('#pro-3', {
-        duration:2000,
-        origin:'left',
-        distance:'30px'
-    });
-
-    sr.reveal('#pro-4', {
-        duration:2000,
-        origin:'right',
-        distance:'30px'
-    });
-
-   
+  sr.reveal("#pro-4", {
+    duration: 2000,
+    origin: "right",
+    distance: "30px"
+  });
 });
