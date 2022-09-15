4debd38be441aea25d81096d505c63e45d327573
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2022-09-15 18:50:25 -0400

    feat(hero): add animation

--- js/script.js
+++ js/script.js
@@ -21,32 +21,34 @@ $(document).ready(function() {
     loop: false
   });
 
-  sr.reveal("#about", {
+  sr.reveal("#hero-description", {
     duration: 2500,
-    origin: "bottom"
-  });
-
-  sr.reveal("#pro-1", {
-    duration: 2000,
-    origin: "left",
+    delay: 150,
+    origin: "bottom",
     distance: "30px"
   });
 
-  sr.reveal("#pro-2", {
-    duration: 2000,
-    origin: "right",
-    distance: "30px"
-  });
+  // sr.reveal("#pro-1", {
+  //   duration: 2000,
+  //   origin: "left",
+  //   distance: "30px"
+  // });
 
-  sr.reveal("#pro-3", {
-    duration: 2000,
-    origin: "left",
-    distance: "30px"
-  });
+  // sr.reveal("#pro-2", {
+  //   duration: 2000,
+  //   origin: "right",
+  //   distance: "30px"
+  // });
 
-  sr.reveal("#pro-4", {
-    duration: 2000,
-    origin: "right",
-    distance: "30px"
-  });
+  // sr.reveal("#pro-3", {
+  //   duration: 2000,
+  //   origin: "left",
+  //   distance: "30px"
+  // });
+
+  // sr.reveal("#pro-4", {
+  //   duration: 2000,
+  //   origin: "right",
+  //   distance: "30px"
+  // });
 });
