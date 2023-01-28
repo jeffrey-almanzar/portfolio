53df0f468b6cb27341a80b3d649336edccc822e7
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2023-01-28 10:45:33 -0500

    feat: initialize skills section

--- js/script.js
+++ js/script.js
@@ -53,3 +53,8 @@ $(document).ready(function() {
   //   distance: "30px"
   // });
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
