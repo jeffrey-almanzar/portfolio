dc7aa3c43648ce8580640798147e065a8da3aa1c
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2023-02-19 13:07:46 -0500

    feat: fix accordions

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ $(document).ready(function () {
   $("#mobile-accordion").accordion({
     collapsible: true,
     heightStyle: "content",
+    beforeActivate,
   });
 
   $("#experience-acc").accordion({
     collapsible: true,
     heightStyle: "content",
+    beforeActivate,
   });
   $("#credentials-acc").accordion({
     collapsible: true,
     heightStyle: "content",
+    beforeActivate,
   });
 
   window.sr = ScrollReveal();
@@ -87,3 +90,29 @@ $(document).ready(function () {
   }
 });
 
+
+function beforeActivate (event, ui) {
+  // The accordion believes a panel is being opened
+  if (ui.newHeader[0]) {
+    var currHeader = ui.newHeader;
+    var currContent = currHeader.next('.ui-accordion-content');
+    // The accordion believes a panel is being closed
+  } else {
+    var currHeader = ui.oldHeader;
+    var currContent = currHeader.next('.ui-accordion-content');
+  }
+  // Since we've changed the default behavior, this detects the actual status
+  var isPanelSelected = currHeader.attr('aria-selected') == 'true';
+
+  // Toggle the panel's header
+  currHeader.toggleClass('ui-corner-all', isPanelSelected).toggleClass('accordion-header-active ui-state-active ui-corner-top', !isPanelSelected).attr('aria-selected', ((!isPanelSelected).toString()));
+
+  // Toggle the panel's icon
+  currHeader.children('.ui-icon').toggleClass('ui-icon-triangle-1-e', isPanelSelected).toggleClass('ui-icon-triangle-1-s', !isPanelSelected);
+
+  // Toggle the panel's content
+  currContent.toggleClass('accordion-content-active', !isPanelSelected)
+  if (isPanelSelected) { currContent.slideUp(); } else { currContent.slideDown(); }
+
+  return false; // Cancels the default action
+}
