6d45ef32ccbbed6d68e7acb3f8386252b1854dbb
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2023-03-04 14:59:08 -0500

    feat: add starting files

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(props) {
+    <footer class="footer py-4 text-center">
+        <div class="container">
+            <p>&copy;<span class="ms-1">Created by Jeffrey Almanzar</span></p>
+        </div>
+    </footer>
+}
+
+export default Footer;
