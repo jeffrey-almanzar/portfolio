80293d4c9ce491d91712df4be3d15c23585bbae9
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2023-03-26 21:51:32 -0400

    feat: style experience section

--- data/credentials.js
+++ data/credentials.js
@@ -2,7 +2,7 @@ function Test(props) {
     return (
         <div>
             {/* <!-- Lehman --> */}
-            <div className="experience d-flex">
+            <div className="experience d-flex mb-3">
                 <div className="logo-container">
                     <a target="_blank"
                         href="http://lehman.smartcatalogiq.com/en/2017-2019/Undergraduate-Bulletin/Academic-Departments-and-Programs/Computer-Science/Computer-Science-B-S-57-61-Credit-Major">
@@ -10,13 +10,13 @@ function Test(props) {
                     </a>
                 </div>
                 <div className="experience-content">
-                    <h3 className="company mb-3">
+                    <h3 className="company mb-2">
                         <a target="_blank"
                             href="http://lehman.smartcatalogiq.com/en/2017-2019/Undergraduate-Bulletin/Academic-Departments-and-Programs/Computer-Science/Computer-Science-B-S-57-61-Credit-Major">Lehman
                             College (CUNY)</a>
                     </h3>
                     <div>
-                        <h4 className="position">
+                        <h4 className="position mb-2">
                             Bachelor of Science in Computer Science
                         </h4>
                         <p className="date-location">2019 - 2020</p>
@@ -24,7 +24,7 @@ function Test(props) {
                 </div>
             </div>
             {/* <!-- BCC --> */}
-            <div className="experience d-flex">
+            <div className="experience d-flex mb-3">
                 <div className="logo-container">
                     <a target="_blank"
                         href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">
@@ -32,13 +32,13 @@ function Test(props) {
                     </a>
                 </div>
                 <div className="experience-content">
-                    <h3 className="company mb-3">
+                    <h3 className="company mb-2">
                         <a target="_blank"
                             href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">Bronx
                             Community College (CUNY)</a>
                     </h3>
                     <div>
-                        <h4 className="position">
+                        <h4 className="position mb-2">
                             Associate of Science in Computer Science
                         </h4>
                         <p className="date-location">2017 - 2018</p>
@@ -52,7 +52,7 @@ function Test2(props) {
     return (
         <div>
             {/* <!-- Google UX Design Certificate --> */}
-            <div className="experience d-flex">
+            <div className="experience d-flex mb-3">
                 <div className="logo-container">
                     <a target="_blank"
                         href="https://grow.google/certificates/ux-design/#?modal_active=none">
@@ -60,32 +60,32 @@ function Test2(props) {
                     </a>
                 </div>
                 <div className="experience-content">
-                    <h3 className="company mb-3">
+                    <h3 className="company mb-2">
                         <a target="_blank"
                             href="https://grow.google/certificates/ux-design/#?modal_active=none">Coursera</a>
                     </h3>
                     <div>
-                        <h4 className="position">Google UX Design Certificate</h4>
+                        <h4 className="position mb-2">Google UX Design Certificate</h4>
                         <p className="date-location">Jun 2022</p>
                     </div>
                 </div>
             </div>
             {/* <!-- Full Stack Web Development  --> */}
-            <div className="experience d-flex">
+            <div className="experience d-flex mb-3">
                 <div className="logo-container">
                     <a target="_blank" href="https://lehmancuny2x.github.io/">
                         <i className="fa-solid fa-building"></i>
                     </a>
                 </div>
                 <div className="experience-content">
-                    <h3 className="company mb-3">
+                    <h3 className="company mb-2">
                         <a target="_blank" href="https://lehmancuny2x.github.io/">Tech Talent
                             Pipiline
                             Recidensy Program at Lehman
                             College</a>
                     </h3>
                     <div>
-                        <h4 className="position">
+                        <h4 className="position mb-2">
                             Full Stack Web Development Certificate
                         </h4>
                         <p className="date-location">Jun 2019</p>
@@ -100,7 +100,7 @@ function Test3(props) {
     return (
         <div>
             {/* <!-- BCC Honor--> */}
-            <div className="experience d-flex">
+            <div className="experience d-flex mb-3">
                 <div className="logo-container">
                     <a target="_blank"
                         href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">
@@ -108,13 +108,13 @@ function Test3(props) {
                     </a>
                 </div>
                 <div className="experience-content">
-                    <h3 className="company mb-3">
+                    <h3 className="company mb-2">
                         <a target="_blank"
                             href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">Bronx
                             Community College</a>
                     </h3>
                     <div>
-                        <h4 className="position">Computer Science Award</h4>
+                        <h4 className="position mb-2">Computer Science Award</h4>
                         <p className="date-location">
                             May 2019 | Computer Science award for outstanding academic
                             achievement in Computer Science.
@@ -123,7 +123,7 @@ function Test3(props) {
                 </div>
             </div>
             {/* <!-- Lehman Honor--> */}
-            <div className="experience d-flex">
+            <div className="experience d-flex mb-3">
                 <div className="logo-container">
                     <a target="_blank"
                         href="http://lehman.smartcatalogiq.com/en/2017-2019/Undergraduate-Bulletin/Academic-Departments-and-Programs/Computer-Science/Computer-Science-B-S-57-61-Credit-Major">
@@ -131,13 +131,13 @@ function Test3(props) {
                     </a>
                 </div>
                 <div className="experience-content">
-                    <h3 className="company mb-3">
+                    <h3 className="company mb-2">
                         <a target="_blank"
                             href="http://lehman.smartcatalogiq.com/en/2017-2019/Undergraduate-Bulletin/Academic-Departments-and-Programs/Computer-Science/Computer-Science-B-S-57-61-Credit-Major">Lehman
                             College</a>
                     </h3>
                     <div>
-                        <h4 className="position">
+                        <h4 className="position mb-2">
                             Guttman Transfer Scholarship Recipient
                         </h4>
                         <p className="date-location">Jan 2019</p>
@@ -145,7 +145,7 @@ function Test3(props) {
                 </div>
             </div>
             {/* <!-- BCC Honor--> */}
-            <div className="experience d-flex">
+            <div className="experience d-flex mb-3">
                 <div className="logo-container">
                     <a target="_blank"
                         href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">
@@ -153,13 +153,13 @@ function Test3(props) {
                     </a>
                 </div>
                 <div className="experience-content">
-                    <h3 className="company mb-3">
+                    <h3 className="company mb-2">
                         <a target="_blank"
                             href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">Bronx
                             Community College</a>
                     </h3>
                     <div>
-                        <h4 className="position">
+                        <h4 className="position mb-2">
                             Bronx Community College Foundation Scholarship Recipient
                         </h4>
                         <p className="date-location">Sep 2018</p>
@@ -167,7 +167,7 @@ function Test3(props) {
                 </div>
             </div>
             {/* <!-- BCC Honor--> */}
-            <div className="experience d-flex">
+            <div className="experience d-flex mb-3">
                 <div className="logo-container">
                     <a target="_blank"
                         href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">
@@ -175,13 +175,13 @@ function Test3(props) {
                     </a>
                 </div>
                 <div className="experience-content">
-                    <h3 className="company mb-3">
+                    <h3 className="company mb-2">
                         <a target="_blank"
                             href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">Bronx
                             Community College</a>
                     </h3>
                     <div>
-                        <h4 className="position">
+                        <h4 className="position mb-2">
                             CUNY-Athletic Conference-Scholar-Athlete Award
                         </h4>
                         <p className="date-location">May 2018</p>
@@ -189,7 +189,7 @@ function Test3(props) {
                 </div>
             </div>
             {/* <!-- BCC Honor--> */}
-            <div className="experience d-flex">
+            <div className="experience d-flex mb-3">
                 <div className="logo-container">
                     <a target="_blank"
                         href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">
@@ -197,13 +197,13 @@ function Test3(props) {
                     </a>
                 </div>
                 <div className="experience-content">
-                    <h3 className="company mb-3">
+                    <h3 className="company mb-2">
                         <a target="_blank"
                             href="http://www.bcc.cuny.edu/academics/academic-departments/mathematics-and-computer-science-department/degree-certificate-courses/computer-science-as/">Bronx
                             Community College</a>
                     </h3>
                     <div>
-                        <h4 className="position">
+                        <h4 className="position mb-2">
                             CUNY-Athletic Conference-Scholar-Athlete Award
                         </h4>
                         <p className="date-location">May 2017</p>
