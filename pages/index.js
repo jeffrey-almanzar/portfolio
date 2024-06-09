75f0fc22b0a650b39bb2b23b5a92c48f5955917f
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2024-06-09 11:10:14 -0400

    feat: animate projects section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css';
+import { motion, useScroll } from "framer-motion"
 
 import { Heading } from '@chakra-ui/react'
 import ProjectCard from '@/components/ProjectCard';
@@ -60,95 +61,126 @@ export default function Home() {
                 <div id="projects-skills" className="group">
                   <div className="project-container">
                     {/* <!-- PROJECT 0 --> */}
-                    <ProjectCard
-                      title="Remote Job Board for JavaScript Developers"
-                      url="https://github.com/Jeffrey-A/remotejavascriptjobs#preview"
-                      leadText="Advanced remote job board that list JavaScript jobs, allowing the user to filter by employment type, development type, main technology, and salary estimation.  In addition, it allows employers to post job ads after making a payment through the integrated Stripe payment gateway."
-                      features={[
-                        'Fetches and displays jobs stored in a Firebase real-time database instance',
-                        'Allows employers to post job ads after making a payment.',
-                        'Integrated the Stripe API to handle payments.',
-                        'Integrated the SendGrid API to send payment confirmation emails.',
-                        'Setup markdown blog using the gray-matter npm package.',
-                        'Dark and light theme modes.',
-                      ]}
-                      skills={[
-                        'HTML',
-                        'CSS',
-                        'JavaScript',
-                        'React JS',
-                        'Next JS',
-                        'Stripe',
-                        'Firebase',
-                        'Sendgrid',
-                      ]}
-                    />
+                    <motion.div
+                      initial={{ x: '100px' }}
+                      whileInView={{ x: '0px' }}
+                      transition={{ duration: 0.7 }}
+                      viewport={{ once: true }}
+                    >
+                      <ProjectCard
+                        title="Remote Job Board for JavaScript Developers"
+                        url="https://github.com/Jeffrey-A/remotejavascriptjobs#preview"
+                        leadText="Advanced remote job board that list JavaScript jobs, allowing the user to filter by employment type, development type, main technology, and salary estimation.  In addition, it allows employers to post job ads after making a payment through the integrated Stripe payment gateway."
+                        features={[
+                          'Fetches and displays jobs stored in a Firebase real-time database instance',
+                          'Allows employers to post job ads after making a payment.',
+                          'Integrated the Stripe API to handle payments.',
+                          'Integrated the SendGrid API to send payment confirmation emails.',
+                          'Setup markdown blog using the gray-matter npm package.',
+                          'Dark and light theme modes.',
+                        ]}
+                        skills={[
+                          'HTML',
+                          'CSS',
+                          'JavaScript',
+                          'React JS',
+                          'Next JS',
+                          'Stripe',
+                          'Firebase',
+                          'Sendgrid',
+                        ]}
+                      />
+                    </motion.div>
+
                     {/* <!-- PROJECT 1 --> */}
-                    <ProjectCard
-                      title="Goals Management App"
-                      url="https://github.com/Jeffrey-A/build-your-future-today#preview"
-                      leadText="Multi-user goals management system that allows users to
+                    <motion.div
+                      initial={{ x: '-100px' }}
+                      whileInView={{ x: '0px' }}
+                      transition={{ duration: 0.7 }}
+                      viewport={{ once: true }}
+                    >
+                      <ProjectCard
+                        title="Goals Management App"
+                        url="https://github.com/Jeffrey-A/build-your-future-today#preview"
+                        leadText="Multi-user goals management system that allows users to
                       create goals, create daily tasks for their goals, and
                       keep track of their progress."
-                      features={[
-                        'Allows creating, editing, and deleting goals from the system.',
-                        'Allows creating, editing, and deleting tasks for each goal.',
-                        'Implemented a Goal Lister page where you can view all the goals at once or filter by name.',
-                        'Implemented a drag and drop goals Board using browser native events.',
-                        'Implemented user authentication, so that a user can create an account and start managing his goals after logging in.',
+                        features={[
+                          'Allows creating, editing, and deleting goals from the system.',
+                          'Allows creating, editing, and deleting tasks for each goal.',
+                          'Implemented a Goal Lister page where you can view all the goals at once or filter by name.',
+                          'Implemented a drag and drop goals Board using browser native events.',
+                          'Implemented user authentication, so that a user can create an account and start managing his goals after logging in.',
 
-                      ]}
-                      skills={[
-                        'HTML',
-                        'CSS',
-                        'JavaScript',
-                        'React JS',
-                        'Node JS',
-                        'Express',
-                        'PostgreSQL',
-                      ]}
-                    />
+                        ]}
+                        skills={[
+                          'HTML',
+                          'CSS',
+                          'JavaScript',
+                          'React JS',
+                          'Node JS',
+                          'Express',
+                          'PostgreSQL',
+                        ]}
+                      />
+
+                    </motion.div>
+
                     {/* <!-- PROJECT 2 --> */}
-                    <ProjectCard
-                      title='Online Shopping Store for Women'
-                      url="https://github.com/Jeffrey-A/eCommerceProject#home"
-                      leadText='Full stack e-commerce with shopping cart functionality,
+                    <motion.div
+                      initial={{ x: '100px' }}
+                      whileInView={{ x: '0px' }}
+                      transition={{ duration: 0.7 }}
+                      viewport={{ once: true }}
+                    >
+                      <ProjectCard
+                        title='Online Shopping Store for Women'
+                        url="https://github.com/Jeffrey-A/eCommerceProject#home"
+                        leadText='Full stack e-commerce with shopping cart functionality,
                       real payment handling and mobile friendly.'
-                      features={[
-                        'Fetches and displays available products stored in a PostgreSQL database.',
-                        'Implemented shopping cart functionality, allowing the user to add multiple products to the cart, select quantity for each product and display the total price to pay.',
-                        'Integrated the Stripe API to handle payments.',
-                      ]}
-                      skills={[
-                        'HTML',
-                        'CSS',
-                        'JavaScript',
-                        'React JS',
-                        'Node JS',
-                        'Express',
-                        'PostgreSQL',
-                      ]}
-                    />
+                        features={[
+                          'Fetches and displays available products stored in a PostgreSQL database.',
+                          'Implemented shopping cart functionality, allowing the user to add multiple products to the cart, select quantity for each product and display the total price to pay.',
+                          'Integrated the Stripe API to handle payments.',
+                        ]}
+                        skills={[
+                          'HTML',
+                          'CSS',
+                          'JavaScript',
+                          'React JS',
+                          'Node JS',
+                          'Express',
+                          'PostgreSQL',
+                        ]}
+                      />
+                    </motion.div>
                     {/* <!-- PROJECT 3 --> */}
-                    <ProjectCard
-                      title='City Search'
-                      url="https://github.com/Jeffrey-A/citySearch#preview"
-                      leadText='When given a city name, it fetches and displays all the
+                    <motion.div
+                      initial={{ x: '-100px' }}
+                      whileInView={{ x: '0px' }}
+                      transition={{ duration: 0.7 }}
+                      viewport={{ once: true }}
+                    >
+                      <ProjectCard
+                        title='City Search'
+                        url="https://github.com/Jeffrey-A/citySearch#preview"
+                        leadText='When given a city name, it fetches and displays all the
                       zip codes of the given city and displays information
                       about each zip code.'
-                      features={[
-                        'Allows the user to input a city name.',
-                        'Gets zip code information for the inputted city from a third party API.',
-                        'Displays all the zip codes for the given city.',
-                        'Displays state, location, population, and total wages for each zip code.',
-                      ]}
-                      skills={[
-                        'HTML',
-                        'CSS',
-                        'JavaScript',
-                        'React JS',
-                      ]}
-                    />
+                        features={[
+                          'Allows the user to input a city name.',
+                          'Gets zip code information for the inputted city from a third party API.',
+                          'Displays all the zip codes for the given city.',
+                          'Displays state, location, population, and total wages for each zip code.',
+                        ]}
+                        skills={[
+                          'HTML',
+                          'CSS',
+                          'JavaScript',
+                          'React JS',
+                        ]}
+                      />
+                    </motion.div>
                   </div>
                 </div>
               </div>
